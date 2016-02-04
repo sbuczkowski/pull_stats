a1027ce5952d7423744c002391427d340d4b6dea
Steven Buczkowski <steven.buczkowski@gmail.com>
2016-02-04 11:52:38 -0500

    Fixed missing loadAddPaths function

---
+++ airs/loadAddPaths_template.m
@@ -0,0 +1,8 @@
+addpath /asl/packages/pull_stats/airs
+addpath /asl/matlib/h4tools
+addpath /asl/rtp_prod/airs/utils
+addpath /asl/packages/rtp_prod2/util
+addpath /home/sergio/MATLABCODE/PLOTTER  %
+                                         % equal_area_spherical_bands
+                                         % (NEED TO MOVE THIS TO
+                                         % MORE CENTRAL LOCATION)
