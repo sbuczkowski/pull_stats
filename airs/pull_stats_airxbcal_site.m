f94da8d35b3774ab0b6f95aaa92c6a95bcc97501
Steven Buczkowski <steven.buczkowski@gmail.com>
2017-03-06 21:30:59 -0500

    Modified site pull_stats for single site
    Previous version read in rtp files for a year, accumulated, and then
    broke out individual sites. This is better for reading the input files
    but blew up memory for the storage arrays. This version works
    memory-wise but requires opening each input rtp file 20 times so,
    watch for file collisions when running on cluster..

--- airs/pull_stats_airxbcal_site.m
+++ airs/pull_stats_airxbcal_site.m
@@ -1,4 +1,4 @@
-function pull_stats_airxbcal_site(year);
+function pull_stats_airxbcal_site(year,siteID);
 
 %*******************************************************************************
 % sequentially read in a year's worth of site rtp files. For each
@@ -28,7 +28,7 @@ basedir = ['/asl/rtp/rtp_airxbcal_v5/' int2str(year) '/site'];
 dayfiles = dir(fullfile(basedir, 'era_airxbcal_day*_site.rtp'));
 fprintf(1,'>>> numfiles = %d\n', length(dayfiles));
 
-nsites = 20;   % use the 20 canonical AIRS site
+% $$$ nsites = 20;   % use the 20 canonical AIRS site
 nchans = 2378; % AIRS channel space
 nlevels = 60;    % ERA
 
@@ -64,28 +64,35 @@ for giday = 1:length(dayfiles)
    % get count of obs at each site
    % selection reason is stored in p.iudef(1,:)
    % site ID is stored in p.iudef(2,:)
-   edges = 0.5:1.0:20.5;
-   [nsiteobs, edges, siteID] = histcounts(p.iudef(2,:), edges);
-   maxobs = max(nsiteobs);  % find site with most obs this day
+% $$$    edges = 0.5:1.0:20.5;
+% $$$    [nsiteobs, edges, siteID] = histcounts(p.iudef(2,:), edges);
+% $$$    maxobs = max(nsiteobs);  % find site with most obs this day
 
-   % allocate space to store this day's obs
-   robs = NaN(nsites, nchans, maxobs, 'single');
-   rcalc = NaN(nsites, nchans, maxobs, 'single');
-   rtimes = NaN(nsites, maxobs, 'double');
-   rlat = NaN(nsites, maxobs, 'single');
-   rlon = NaN(nsites, maxobs, 'single');
-   satzen = NaN(nsites, maxobs, 'single');
-   solzen = NaN(nsites, maxobs, 'single');
-   spres = NaN(nsites, maxobs, 'single');
-   stemp = NaN(nsites, maxobs, 'single');
-   ptemp = NaN(nsites, nlevels, maxobs, 'single');
-   gas_1 = NaN(nsites, nlevels, maxobs, 'single');
-   gas_3 = NaN(nsites, nlevels, maxobs, 'single');
-   iudef1 = NaN(nsites, maxobs, 'single');
-   iudef2 = NaN(nsites, maxobs, 'single');
-   iudef4 = NaN(nsites, maxobs, 'single');
-   nlevs = NaN(nsites, maxobs, 'single');
-   plevs = NaN(nsites, nlevels, maxobs, 'single');
+   siteind = find(p.iudef(2,:) == siteID);
+   nsiteobs = length(siteind);
+
+   if ~nsiteobs
+       continue;
+   end
+   
+% $$$    % allocate space to store this day's obs
+% $$$    robs = NaN(nchans, nsiteobs, 'single');
+% $$$    rcalc = NaN(nchans, nsiteobs, 'single');
+% $$$    rtimes = NaN(nsiteobs, 'double');
+% $$$    rlat = NaN(nsiteobs, 'single');
+% $$$    rlon = NaN(nsiteobs, 'single');
+% $$$    satzen = NaN(nsiteobs, 'single');
+% $$$    solzen = NaN(nsiteobs, 'single');
+% $$$    spres = NaN(nsiteobs, 'single');
+% $$$    stemp = NaN(nsiteobs, 'single');
+% $$$    ptemp = NaN(nlevels, nsiteobs, 'single');
+% $$$    gas_1 = NaN(nlevels, nsiteobs, 'single');
+% $$$    gas_3 = NaN(nlevels, nsiteobs, 'single');
+% $$$    iudef1 = NaN(nsiteobs, 'single');
+% $$$    iudef2 = NaN(nsiteobs, 'single');
+% $$$    iudef4 = NaN(nsiteobs, 'single');
+% $$$    nlevs = NaN(nsiteobs, 'single');
+% $$$    plevs = NaN(nlevels, nsiteobs, 'single');
 
    % Look for bad channels and initialize counts
    [nedt,ab,ical] = calnum_to_data(p.calflag,cstr);
@@ -100,114 +107,37 @@ for giday = 1:length(dayfiles)
        p.robs1(i,k) = NaN;
        count_all(i,k) = 0;
    end   % for i=1:2378
-
-   % loop over sites
-   for s = 1:nsites
-       if ~nsiteobs   % no obs for this site. move to next
-           continue;  
-       end   % if ~nsiteobs
-       
-       siteind = find(siteID == s);
-       rtimes(s,1:nsiteobs(s)) = p.rtime(siteind);
-       robs(s,:,1:nsiteobs(s)) = p.robs1(:,siteind);
-       rcalc(s,:,1:nsiteobs(s)) = p.rcalc(:,siteind);
-       rlat(s,1:nsiteobs(s)) = p.rlat(siteind);
-       rlon(s,1:nsiteobs(s)) = p.rlon(siteind);
-       satzen(s,1:nsiteobs(s)) = p.satzen(siteind);
-       solzen(s,1:nsiteobs(s)) = p.solzen(siteind); 
-       spres(s,1:nsiteobs(s)) = p.spres(siteind);
-       stemp(s,1:nsiteobs(s)) = p.stemp(siteind);
-       ptemp(s,:,1:nsiteobs(s)) = p.ptemp(:,siteind);
-       gas_1(s,:,1:nsiteobs(s)) = p.gas_1(:,siteind);
-       gas_3(s,:,1:nsiteobs(s)) = p.gas_3(:,siteind);
-       iudef1(s,1:nsiteobs(s)) = p.iudef(1,siteind); 
-       iudef2(s,1:nsiteobs(s)) = p.iudef(2,siteind);
-       iudef4(s,1:nsiteobs(s)) = p.iudef(4,siteind); 
-       nlevs(s,1:nsiteobs(s)) = p.nlevs(siteind);
-       plevs(s,:,1:nsiteobs(s)) = p.plevs(:,siteind);
-   
-   end   % for s = 1:20
-
+    
    % accumulate into the annual tally
-   accum_rtime = cat(2, accum_rtime, rtimes);
-   accum_robs = cat(3, accum_robs, robs);
-   accum_rcalc = cat(3, accum_rcalc, rcalc);
-   accum_rlat = cat(2, accum_rlat, rlat);
-   accum_rlon = cat(2, accum_rlon, rlon);
-   accum_satzen = cat(2, accum_satzen, satzen);
-   accum_solzen = cat(2, accum_solzen, solzen);
-   accum_spres = cat(2, accum_spres, spres);
-   accum_stemp = cat(2, accum_stemp, stemp);
-   accum_ptemp = cat(3, accum_ptemp, ptemp);
-   accum_gas_1 = cat(3, accum_gas_1, gas_1);
-   accum_gas_3 = cat(3, accum_gas_3, gas_3);
-   accum_iudef1 = cat(2, accum_iudef1, iudef1);
-   accum_iudef2 = cat(2, accum_iudef2, iudef2);
-   accum_iudef4 = cat(2, accum_iudef4, iudef4);
-   accum_nlevs = cat(2, accum_nlevs, nlevs);
-   accum_plevs = cat(3, accum_plevs, plevs);
+   accum_rtime = cat(2, accum_rtime, p.rtime(siteind));  
+   accum_robs = cat(2, accum_robs, p.robs1(:,siteind));
+   accum_rcalc = cat(2, accum_rcalc, p.rcalc(:,siteind));
+   accum_rlat = cat(2, accum_rlat, p.rlat(siteind));   
+   accum_rlon = cat(2, accum_rlon, p.rlon(siteind));   
+   accum_satzen = cat(2, accum_satzen, p.satzen(siteind)); 
+   accum_solzen = cat(2, accum_solzen, p.solzen(siteind)); 
+   accum_spres = cat(2, accum_spres, p.spres(siteind));  
+   accum_stemp = cat(2, accum_stemp, p.stemp(siteind));  
+   accum_ptemp = cat(2, accum_ptemp, p.ptemp(:,siteind));
+   accum_gas_1 = cat(2, accum_gas_1, p.gas_1(:,siteind));
+   accum_gas_3 = cat(2, accum_gas_3, p.gas_3(:,siteind));
+   accum_iudef1 = cat(2, accum_iudef1, p.iudef(1,siteind));
+   accum_iudef2 = cat(2, accum_iudef2, p.iudef(2,siteind));
+   accum_iudef4 = cat(2, accum_iudef4, p.iudef(4,siteind));
+   accum_nlevs = cat(2, accum_nlevs, p.nlevs(siteind));  
+   accum_plevs = cat(2, accum_plevs, p.plevs(:,siteind));
    
    iday = iday + 1
    end   % if a.bytes > 1000000
 
 end  % giday
 
-% clear variables for re-use (data /should/ just overwrite but,
-% just to be safe)
-clear rtimes robs rcalc rlat rlon satzen solzen spres stemp ptemp ...
-    gas_1 gas_3 iudef1 iudef2 iudef4 nlevs plevs
-
 % $$$ outfileDir='/asl/data/stats/airs';
 outfileDir='/home/sbuczko1/WorkingFiles/data/stats/airs';
 
-% loop over sites and break out individual tallies to separate
-% files
-for s=1:nsites
-    % pull current site out of accumulated arrays
-    rtimes = squeeze(accum_rtime(s,:));
-    robs = squeeze(accum_robs(s,:,:));
-    rcalc = squeeze(accum_rcalc(s,:,:));
-    rlat = squeeze(accum_rlat(s,:));
-    rlon = squeeze(accum_rlon(s,:));
-    satzen = squeeze(accum_satzen(s,:));
-    solzen = squeeze(accum_solzen(s,:));
-    spres = squeeze(accum_spres(s,:));
-    stemp = squeeze(accum_stemp(s,:));
-    ptemp = squeeze(accum_ptemp(s,:,:));
-    gas_1 = squeeze(accum_gas_1(s,:,:));
-    gas_3 = squeeze(accum_gas_3(s,:,:));
-    iudef1 = squeeze(accum_iudef1(s,:));
-    iudef2 = squeeze(accum_iudef2(s,:));
-    iudef4 = squeeze(accum_iudef4(s,:));
-    nlevs = squeeze(accum_nlevs(s,:));
-    plevs = squeeze(accum_plevs(s,:,:));
-
-    % find NaN entries in obs dimension
-    removeIndices = isnan(rtimes);
-
-    % remove NaN obs from arrays
-    rtimes(removeIndices) = [];
-    robs(:,removeIndices) = [];
-    rcalc(:,removeIndices) = [];
-    rlat(removeIndices) = [];
-    rlon(removeIndices) = [];
-    satzen(removeIndices) = []; 
-    solzen(removeIndices) = [];
-    spres(removeIndices) = [];
-    stemp(removeIndices) = [];
-    ptemp(:,removeIndices) = [];
-    gas_1(:,removeIndices) = [];
-    gas_3(:,removeIndices) = [];
-    iudef1(removeIndices) = []; 
-    iudef2(removeIndices) = []; 
-    iudef4(removeIndices) = []; 
-    nlevs(removeIndices) = [];
-    plevs(:,removeIndices) = [];
-
-    outfileName = sprintf('airxbcal_site-%d_%d_era_rad_stats', s, year);
-    outfilePath = fullfile(outfileDir, outfileName)
-    eval_str = sprintf(['save %s rtimes robs rcalc rlat rlon *zen ' ...
-                        'gas_* iudef* *levs *temp spres'], outfilePath);
-    eval(eval_str);
+outfileName = sprintf('airxbcal_site-%d_%d_era_rad_stats', siteID, year);
+outfilePath = fullfile(outfileDir, outfileName)
+eval_str = sprintf('save %s accum_*', outfilePath);
+eval(eval_str);
 end
 
