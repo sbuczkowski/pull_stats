82bb340e638595ab289e70db16d501f47cf8b058
Steven Buczkowski <steven.buczkowski@gmail.com>
2018-03-21 17:37:19 -0400

    added latlimit config keyword to random_fs scanangle acc. files

--- airs/run_pull_stats_airicrad_rand_fs_scanang.m
+++ airs/run_pull_stats_airicrad_rand_fs_scanang.m
@@ -5,9 +5,10 @@ addpath(genpath(rtp_sw_dir));
 % grab the slurm array index for this process
 slurmindex = str2num(getenv('SLURM_ARRAY_TASK_ID'));
 
-cfg.rtpdir = '/home/sbuczko1/WorkingFiles/rtp_airicrad_v6';
-cfg.statsdir = '/home/sbuczko1/WorkingFiles/data/stats/airs';
+%cfg.rtpdir = '/home/sbuczko1/WorkingFiles/rtp_airicrad_v6';
+%cfg.statsdir = '/home/sbuczko1/WorkingFiles/data/stats/airs';
 cfg.klayers = true;
+cfg.latlimit = 90; % include all obs
 
 year = 2002+slurmindex;
 disp(year)
